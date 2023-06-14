e4e90df67c2d350733d0e2658eb5ea4c224b9058
Nick Wilson <nicknwilson@outlook.com>
2023-06-14 01:23:20 +0100

    button added for toggle

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,14 @@ import {
   Image,
   Message,
   Label,
+  Button,
   Menu,
   Card,
 } from "semantic-ui-react";
 
 import SideGroup from "./components/SideGroup";
 import { personal_data, work_data, education_data } from "./data";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 
 const languages = [
   "Python",
@@ -66,15 +67,32 @@ const python = [
 const db = ["Snowflake", "SQLite", "Postgres", "Neo4J", "MongoDB", "Firebase"];
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const setDark = () => {
+    setTheme("dark");
+  };
+
+  const setLight = () => {
+    document.body.style.backgroundColor = "#f5f5f5";
+    setTheme("light");
+  };
+
   useLayoutEffect(() => {
     document.body.style.backgroundColor = "#f5f5f5";
   });
+
   return (
     <div className="App">
-      <Container style={{ paddingTop: "1rem", paddingBottom: "1rem" }}>
-        {" "}
+      <Container
+        style={{
+          paddingTop: "1rem",
+          paddingBottom: "1rem",
+          color: theme === "dark" ? "white" : "black",
+        }}
+      >
         <Grid columns="equal" stackable container relaxed="very">
-          <Grid.Column width={8} style={{ margin: "0px", padding: "2rem" }}>
+          <Grid.Column width={9} style={{ margin: "0px", padding: "2rem" }}>
             <Card
               centered
               style={{
@@ -90,29 +108,65 @@ function App() {
                   alt="image"
                   style={{ width: "6rem" }}
                 ></Image>
+                <Segment
+                  textAlign="center"
+                  style={{
+                    paddingLeft: "0.2rem",
+                    paddingRight: "0.2rem",
+                    paddingTop: "0.5rem",
+                    paddingBottom: "0.2rem",
+                    border: "none",
+                    boxShadow: "none",
+                    margin: "0px",
+                  }}
+                >
+                  <Button.Group>
+                    <Button onClick={setDark}>Dark Mode</Button>
+                    <Button.Or />
+                    <Button onClick={setLight}>Light Mode</Button>
+                  </Button.Group>
+                </Segment>{" "}
               </Header>
               <Card.Content>
-                <Card.Header>Nicholas N. Wilson Software Portfolio</Card.Header>
+                <Card.Header
+                  style={{ color: theme === "dark" ? "white" : "black" }}
+                >
+                  Nicholas N. Wilson Software Portfolio
+                </Card.Header>
                 <Card.Meta>Last updated 08/06/2023</Card.Meta>{" "}
               </Card.Content>{" "}
               <Card.Content extra>
                 <Card.Description>
-                  <b>Actively Seeking Employment In The Following Roles:</b>
+                  <b style={{ color: theme === "dark" ? "white" : "black" }}>
+                    Actively Seeking Employment In The Following Roles:
+                  </b>
                 </Card.Description>
                 <div style={{ margin: "5px" }}></div>
 
-                <Label as="a" basic>
+                <Label
+                  as="a"
+                  basic
+                  style={{ color: theme === "dark" ? "white" : "black" }}
+                >
                   Data Engineer
                 </Label>
-                <Label as="a" basic>
+                <Label
+                  as="a"
+                  basic
+                  style={{ color: theme === "dark" ? "white" : "black" }}
+                >
                   Python Engineer
                 </Label>
-                <Label as="a" basic>
+                <Label
+                  as="a"
+                  basic
+                  style={{ color: theme === "dark" ? "white" : "black" }}
+                >
                   Software Engineer
                 </Label>
 
                 <Message>
-                  <p>
+                  <p style={{ color: theme === "dark" ? "white" : "black" }}>
                     After leaving my previous role at the end of 2022 I have
                     been self-employed for six months. I am now seeking a new
                     role as a python engineer in data/software. I am a
@@ -182,8 +236,16 @@ function App() {
             paddingRight: "0.2rem",
             paddingTop: "0px",
             paddingBottom: "2rem",
+            backgroundColor: theme === "dark" ? "#444447" : "white",
           }}
         >
+          <Grid.Column
+            width={2}
+            textAlign="center"
+            style={{ padding: "0px", margin: "0px" }}
+          >
+            {" "}
+          </Grid.Column>
           <Grid stackable container columns={2} relaxed="very">
             <Grid.Column>
               <Grid.Row>
